13ad8827d2d7915a76d852210981ed7da0f6c637
Kate <kit.ty.kate@disroot.org>
2021-02-10 01:39:56 +0000

    Remove unused module

--- lib/buildkit_syntax.mli
+++
@@ -1,4 +0,0 @@
-val add : Ocaml_version.arch -> Dockerfile.t
-(** [add arch] will activate BuildKit experimental syntax with
-    a hash that will work for that architecture. Defaults to x86_64
-    if no arch is specified. *)
