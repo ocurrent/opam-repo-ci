6ea493b8bf31ed5a4dee19956401ef287274de16
Kate <kit.ty.kate@disroot.org>
2021-03-02 16:23:53 +0000

    Add nnpchecker to the pipeline

--- service/pipeline.ml
+++ service/pipeline.ml
@@ -194,8 +194,10 @@ let build_with_cluster ~ocluster ~analysis ~lint ~master source =
     let master_distro = Dockerfile_distro.tag_of_distro master_distro in
     let default_compiler = Ocaml_version.to_string default_compiler in
     let flambda = Variant.v ~arch:`X86_64 ~distro:master_distro ~compiler:(default_compiler, Some "flambda") in
+    let nnpchecker = Variant.v ~arch:`X86_64 ~distro:master_distro ~compiler:(default_compiler, Some "nnpchecker") in
     Current.list_seq (
       build ~upgrade_opam:true ~revdeps:false "flambda" flambda ::
+      build ~upgrade_opam:true ~revdeps:false "nnpchecker" nnpchecker ::
       List.fold_left (fun acc arch ->
         if arch = `X86_64 then
           acc
