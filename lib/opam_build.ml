ab197e4423a62a90e3070802b5d111750bb9be47
Kate <kit.ty.kate@disroot.org>
2021-07-19 15:44:21 +0100

    hotfix

--- lib/opam_build.ml
+++ lib/opam_build.ml
@@ -76,6 +76,7 @@ let setup_repository ~variant ~for_docker ~opam_version =
   env "OPAMERRLOGLEN" "0" :: (* Show the whole log if it fails *)
   env "OPAMSOLVERTIMEOUT" "500" :: (* Increase timeout. Poor mccs is doing its best *)
   env "OPAMPRECISETRACKING" "1" :: (* Mitigate https://github.com/ocaml/opam/issues/3997 *)
+  (if Variant.is_macos variant then [] else [workdir "/home/opam"]) @
   [
     run "rm -rf opam-repository/";
     copy ["."] ~dst:"opam-repository/";
