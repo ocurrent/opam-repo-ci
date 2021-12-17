9cefa639ca51fb664ed6490be4913f740565602a
Kate <kit.ty.kate@disroot.org>
2021-12-17 17:56:07 +0000

    Increase the job pool limit to 1000

--- lib/build.ml
+++ lib/build.ml
@@ -120,7 +120,7 @@ module Op = struct
 
   module Outcome = Current.String
 
-  let lwt_pool = Lwt_pool.create 125 (fun () -> Lwt.return_unit)
+  let lwt_pool = Lwt_pool.create 1000 (fun () -> Lwt.return_unit)
 
   let run { config = { connection; timeout }; master; urgent; base } job { Key.pool; commit; variant; ty } () =
     Lwt_pool.use lwt_pool @@ fun () ->
