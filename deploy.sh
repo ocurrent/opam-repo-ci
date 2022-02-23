785b911ea89f63e33a44dc0c7f3f187d1fb1308e
Tim McGilchrist <timmcgil@gmail.com>
2022-02-23 14:08:41 +1100

    Fix stack name in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 docker build -t opam-repo-ci-web -f Dockerfile.web .
 docker build -t opam-repo-ci-service -f Dockerfile .
-docker stack rm opam-ci
+docker stack rm opam-repo-ci
 sleep 15
-docker stack deploy -c stack.yml opam-ci
+docker stack deploy -c stack.yml opam-repo-ci
