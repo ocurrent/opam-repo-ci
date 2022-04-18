a28dd51a6623900dbf6568febb8060036061636b
Kate <kit.ty.kate@disroot.org>
2022-04-18 16:06:30 +0100

    Make the generic opam error pattern stricter to avoid an issue with ocurrent log matcher
    For some reason the pattern would take over in all cases even if other pattern with higher score would match

--- service/main.ml
+++ service/main.ml
@@ -86,7 +86,7 @@ let add_default_matching_log_rules () =
         score = 30;
       };
       { (* Opam errors *)
-        pattern = {|[\n]\[ERROR\] (.+)[\n]|};
+        pattern = {|[\n]\[ERROR\] (.+)\.[\n]|};
         report = {|\1|};
         score = 20;
       };
