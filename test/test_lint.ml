b2560ef15aa9acdb70fb8ed304ce82024db54246
benmandrew <benmandrew@gmail.com>
2024-02-22 18:02:27 +0100

    Added test cases and comments

--- test/test_lint.ml
+++ test/test_lint.ml
@@ -28,14 +28,14 @@ open Git_util
 let check_result label expected stream =
   Lwt_stream.get stream >|= fun value ->
   let value = Option.map (Result.map_error (fun (`Msg s) -> s)) value in
-  Alcotest.(check (option (result unit string)) label value (Some expected))
+  Alcotest.(check (option (result unit string)) label (Some expected) value)
 
 let with_temp_repo f =
   Lwt_io.with_temp_dir ~prefix:"dummy-opam-repository-" @@
   fun repo_dir ->
   let repo_dir = Fpath.v repo_dir in
   init repo_dir >>= fun () ->
-  apply_patches ~cwd:repo_dir "a-a" [ "a.patch" ] >>= fun () ->
+  apply_patches ~cwd:repo_dir "a-1" [ "a-1.patch" ] >>= fun () ->
   Cmd.git ~cwd:repo_dir [ "checkout"; "-qb"; "new-branch" ] >>= fun () ->
   f repo_dir
 
@@ -49,22 +49,29 @@ let test_correct _switch () =
   check_result "Correct" (Ok ()) results >>= fun () ->
   shutdown_pipeline engine
 
-let test_no_author _switch () =
+(** Tests the following:
+    - [b.0.0.1] is missing the [author] field
+    - [b.0.0.2] has an extra unknown field
+    - [b.0.0.3] is correct *)
+let test_incorrect_opam _switch () =
   with_temp_repo @@ fun repo_dir ->
   let engine = Current.Engine.create @@ pipeline repo_dir in
-  apply_patches ~cwd:repo_dir "b-no-author" [ "b-no-author.patch" ] >>= fun () ->
-  check_result "No author" (Error "3 errors") results >>= fun () ->
+  apply_patches ~cwd:repo_dir "b-incorrect-opam" [ "b-incorrect-opam.patch" ] >>= fun () ->
+  check_result "Incorrect opam" (Error "2 errors") results >>= fun () ->
   shutdown_pipeline engine
 
-let test_unknown_field _switch () =
+(** Tests the package name collision detection by adding four versions
+    of a package [a_1] that conflicts with the existing [a-1] package *)
+let test_name_collision _switch () =
   with_temp_repo @@ fun repo_dir ->
   let engine = Current.Engine.create @@ pipeline repo_dir in
-  apply_patches ~cwd:repo_dir "b-unknown-field" [ "b-unknown-field.patch" ] >>= fun () ->
-  check_result "Unknown field" (Error "3 errors") results >>= fun () ->
+  apply_patches ~cwd:repo_dir "a_1-name-collision" [ "a_1-name-collision.patch" ] >>= fun () ->
+  check_result "Package name collision" (Error "4 errors") results >>= fun () ->
   shutdown_pipeline engine
 
+(** Tests are marked [`Slow] as the [Lint.check] call can take a few seconds *)
 let tests = [
     Alcotest_lwt.test_case "correct" `Slow test_correct;
-    Alcotest_lwt.test_case "no-author" `Slow test_no_author;
-    Alcotest_lwt.test_case "unknown-field" `Slow test_unknown_field;
+    Alcotest_lwt.test_case "incorrect-opam" `Slow test_incorrect_opam;
+    Alcotest_lwt.test_case "name-collision" `Slow test_name_collision;
   ]
