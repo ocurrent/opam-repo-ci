punchagan/revdeps-prototype

Add dune version constraint check
This is just a dump of a bunch of code from opam-repo-ci stripping out
the Lwt bits to simplify it.

lib/lint.ml
@@ -1,3 +1,5 @@
+module D = Dir_helpers
+
 module Checks = struct
   type error =
     | UnnecessaryField of string
@@ -7,6 +9,11 @@ module Checks = struct
     | WeakChecksum of string
     | PinDepends
     | ExtraFiles
+    | FailedToDownload of string
+    | DuneProjectMissing
+    | DuneConstraintMissing
+    | BadDuneConstraint of string * string
+    | DuneIsBuild
 
   let check_name_field ~pkg opam =
     match OpamFile.OPAM.name_opt opam with
@@ -94,11 +101,114 @@ module Checks = struct
     | None | Some [] -> []
     | Some _ -> [ (pkg, ExtraFiles) ]
 
+  let with_temp_dir prefix f =
+    let dir = D.create_temp_dir prefix in
+    let res = f dir in
+    D.remove_dir dir;
+    res
+
+  let get_dune_project_version ~pkg url =
+    with_temp_dir "lint-dune-project-version-" @@ fun dir ->
+    let res =
+      OpamProcess.Job.run
+      @@ OpamRepository.pull_tree
+           (OpamPackage.to_string pkg)
+           (OpamFilename.Dir.of_string dir)
+           (OpamFile.URL.checksum url)
+           [ OpamFile.URL.url url ]
+    in
+    match res with
+    | OpamTypes.Not_available (_, msg) -> Error msg
+    | Up_to_date _ | Result _ -> (
+        let dune_project = Filename.concat dir "dune-project" in
+        try
+          let ch = open_in dune_project in
+          try
+            let content = really_input_string ch (in_channel_length ch) in
+            close_in ch;
+            match Sexplib.Sexp.parse content with
+            | Sexplib.Sexp.Done
+                (List [ Atom "lang"; Atom "dune"; Atom version ], _) ->
+                Ok (Some version)
+            | Done _ -> failwith "(lang dune ...) is not the first construct"
+            | Cont _ -> failwith "Failed to parse the dune-project file"
+          with e ->
+            close_in_noerr ch;
+            raise e
+        with
+        | Sys_error _ -> Ok None
+        | e -> raise e)
+
+  let is_dune name =
+    OpamPackage.Name.equal name (OpamPackage.Name.of_string "dune")
+
+  let get_dune_constraint opam =
+    let get_max = function
+      | None, None -> None
+      | Some x, None -> Some x
+      | None, Some x -> Some x
+      | Some x, Some y when OpamVersionCompare.compare x y >= 0 -> Some x
+      | Some _, Some y -> Some y
+    in
+    let get_min = function
+      | None, None | Some _, None | None, Some _ -> None
+      | Some x, Some y when OpamVersionCompare.compare x y >= 0 -> Some y
+      | Some x, Some _ -> Some x
+    in
+    let is_build = ref false in
+    let rec get_lower_bound = function
+      | OpamFormula.Atom
+          (OpamTypes.Constraint ((`Gt | `Geq | `Eq), OpamTypes.FString version))
+        ->
+          Some version
+      | Atom (Filter (FIdent (_, var, _)))
+        when String.equal (OpamVariable.to_string var) "build" ->
+          is_build := true;
+          None (* TODO: remove this hack *)
+      | Empty | Atom (Filter _) | Atom (Constraint _) -> None
+      | Block x -> get_lower_bound x
+      | And (x, y) -> get_max (get_lower_bound x, get_lower_bound y)
+      | Or (x, y) -> get_min (get_lower_bound x, get_lower_bound y)
+    in
+    let rec aux = function
+      | OpamFormula.Atom (pkg, constr) ->
+          if is_dune pkg then
+            let v = get_lower_bound constr in
+            Some (Option.default "1.0" v)
+          else None
+      | Empty -> None
+      | Block x -> aux x
+      | And (x, y) -> get_max (aux x, aux y)
+      | Or (x, y) -> get_min (aux x, aux y)
+    in
+    (!is_build, aux opam.OpamFile.OPAM.depends)
+
+  let check_dune_constraints ~pkg opam =
+    match opam.OpamFile.OPAM.url with
+    | Some url ->
+        let dune_version = get_dune_project_version ~pkg url in
+        let is_build, dune_constraint = get_dune_constraint opam in
+        let errors =
+          match (dune_constraint, dune_version) with
+          | _, Error msg -> [ (pkg, FailedToDownload msg) ]
+          | None, Ok None -> []
+          | Some _, Ok None -> [ (pkg, DuneProjectMissing) ]
+          | None, Ok (Some _) ->
+              if is_dune (OpamPackage.name pkg) then []
+              else [ (pkg, DuneConstraintMissing) ]
+          | Some dep, Ok (Some ver) ->
+              if OpamVersionCompare.compare dep ver >= 0 then []
+              else [ (pkg, BadDuneConstraint (dep, ver)) ]
+        in
+        if is_build then [ (pkg, DuneIsBuild) ] else errors
+    | None -> []
+
   let checks =
     [
       check_name_field;
       check_version_field;
       check_dune_subst;
+      check_dune_constraints;
       check_checksums;
       check_no_pin_depends;
       check_no_extra_files;
@@ -150,6 +260,32 @@ let msg_of_errors (package, (err : Checks.error)) =
       Printf.sprintf
         "Error in %s: extra-files present. This is not allowed in the \
          opam-repository. Please use extra-source instead."
+        pkg
+  | FailedToDownload msg ->
+      Printf.sprintf "Error in %s: Failed to download the archive. Details: %s"
+        pkg msg
+  | DuneProjectMissing ->
+      Printf.sprintf
+        "Warning in %s: The package seems to use dune but the dune-project \
+         file is missing."
+        pkg
+  | DuneConstraintMissing ->
+      Printf.sprintf
+        "Warning in %s: The package has a dune-project file but no explicit \
+         dependency on dune was found."
+        pkg
+  | BadDuneConstraint (dep, ver) ->
+      Printf.sprintf
+        "Error in %s: Your dune-project file indicates that this package \
+         requires at least dune %s but your opam file only requires dune >= \
+         %s. Please check which requirement is the right one, and fix the \
+         other."
+        pkg ver dep
+  | DuneIsBuild ->
+      Printf.sprintf
+        "Warning in %s: The package tagged dune as a build dependency. Due to \
+         a bug in dune (https://github.com/ocaml/dune/issues/2147) this should \
+         never be the case. Please remove the {build} tag from its filter."
         pkg
 
 let ( // ) = Filename.concat

ocurrent/opam-repo-ci

lib/lint.ml
@@ -1,9 +1,16 @@
+module D = Dir_helpers
+
 module Checks = struct
   type error =
     | UnnecessaryField of string
     | UnmatchedName of OpamPackage.Name.t
     | UnmatchedVersion of OpamPackage.Version.t
     | DubiousDuneSubst
+    | DuneProjectMissing
+    | DuneConstraintMissing
+    | DuneIsBuild
+    | BadDuneConstraint of string * string
+    | FailedToDownload of string
     | WeakChecksum of string
     | PinDepends
     | ExtraFiles
@@ -94,11 +101,104 @@ module Checks = struct
     | None | Some [] -> []
     | Some _ -> [ (pkg, ExtraFiles) ]
 
+  let get_dune_project_version ~pkg url =
+    D.with_temp_dir "lint-dune-project-version-" @@ fun dir ->
+    let res =
+      OpamProcess.Job.run
+      @@ OpamRepository.pull_tree
+           (OpamPackage.to_string pkg)
+           (OpamFilename.Dir.of_string dir)
+           (OpamFile.URL.checksum url)
+           [ OpamFile.URL.url url ]
+    in
+    match res with
+    | OpamTypes.Not_available (_, msg) -> Error msg
+    | Up_to_date _ | Result _ -> (
+        let dune_project = Filename.concat dir "dune-project" in
+        match
+          In_channel.input_all
+          |> In_channel.with_open_text dune_project
+          |> Sexplib.Sexp.parse
+        with
+        | exception Sys_error _ -> Ok None
+        | Sexplib.Sexp.Done (List [ Atom "lang"; Atom "dune"; Atom version ], _)
+          ->
+            Ok (Some version)
+        | Done _ -> Error "(lang dune ...) is not the first construct"
+        | Cont _ -> Error "Failed to parse the dune-project file")
+
+  let is_dune name =
+    OpamPackage.Name.equal name (OpamPackage.Name.of_string "dune")
+
+  let get_dune_constraint opam =
+    let get_max = function
+      | None, None -> None
+      | Some x, None -> Some x
+      | None, Some x -> Some x
+      | Some x, Some y when OpamVersionCompare.compare x y >= 0 -> Some x
+      | Some _, Some y -> Some y
+    in
+    let get_min = function
+      | None, None | Some _, None | None, Some _ -> None
+      | Some x, Some y when OpamVersionCompare.compare x y >= 0 -> Some y
+      | Some x, Some _ -> Some x
+    in
+    let is_build = ref false in
+    let rec get_lower_bound = function
+      | OpamFormula.Atom
+          (OpamTypes.Constraint ((`Gt | `Geq | `Eq), OpamTypes.FString version))
+        ->
+          Some version
+      | Atom (Filter (FIdent (_, var, _)))
+        when String.equal (OpamVariable.to_string var) "build" ->
+          (* TODO: remove this hack-ish side-effect change setting is_build, in
+             function to get version lower bound *)
+          is_build := true;
+          None
+      | Empty | Atom (Filter _) | Atom (Constraint _) -> None
+      | Block x -> get_lower_bound x
+      | And (x, y) -> get_max (get_lower_bound x, get_lower_bound y)
+      | Or (x, y) -> get_min (get_lower_bound x, get_lower_bound y)
+    in
+    let rec aux = function
+      | OpamFormula.Atom (pkg, constr) ->
+          if is_dune pkg then
+            let v = get_lower_bound constr in
+            Some (Option.default "1.0" v)
+          else None
+      | Empty -> None
+      | Block x -> aux x
+      | And (x, y) -> get_max (aux x, aux y)
+      | Or (x, y) -> get_min (aux x, aux y)
+    in
+    (!is_build, aux opam.OpamFile.OPAM.depends)
+
+  let check_dune_constraints ~pkg opam =
+    match opam.OpamFile.OPAM.url with
+    | Some url ->
+        let dune_version = get_dune_project_version ~pkg url in
+        let is_build, dune_constraint = get_dune_constraint opam in
+        let errors =
+          match (dune_constraint, dune_version) with
+          | _, Error msg -> [ (pkg, FailedToDownload msg) ]
+          | None, Ok None -> []
+          | Some _, Ok None -> [ (pkg, DuneProjectMissing) ]
+          | None, Ok (Some _) ->
+              if is_dune (OpamPackage.name pkg) then []
+              else [ (pkg, DuneConstraintMissing) ]
+          | Some dep, Ok (Some ver) ->
+              if OpamVersionCompare.compare dep ver >= 0 then []
+              else [ (pkg, BadDuneConstraint (dep, ver)) ]
+        in
+        if is_build then [ (pkg, DuneIsBuild) ] else errors
+    | None -> []
+
   let checks =
     [
       check_name_field;
       check_version_field;
       check_dune_subst;
+      check_dune_constraints;
       check_checksums;
       check_no_pin_depends;
       check_no_extra_files;
@@ -150,6 +250,32 @@ let msg_of_error (package, (err : Checks.error)) =
       Printf.sprintf
         "Error in %s: extra-files present. This is not allowed in the \
          opam-repository. Please use extra-source instead."
+        pkg
+  | FailedToDownload msg ->
+      Printf.sprintf "Error in %s: Failed to download the archive. Details: %s"
+        pkg msg
+  | DuneProjectMissing ->
+      Printf.sprintf
+        "Warning in %s: The package seems to use dune but the dune-project \
+         file is missing."
+        pkg
+  | DuneConstraintMissing ->
+      Printf.sprintf
+        "Warning in %s: The package has a dune-project file but no explicit \
+         dependency on dune was found."
+        pkg
+  | BadDuneConstraint (dep, ver) ->
+      Printf.sprintf
+        "Error in %s: Your dune-project file indicates that this package \
+         requires at least dune %s but your opam file only requires dune >= \
+         %s. Please check which requirement is the right one, and fix the \
+         other."
+        pkg ver dep
+  | DuneIsBuild ->
+      Printf.sprintf
+        "Warning in %s: The package tagged dune as a build dependency. Due to \
+         a bug in dune (https://github.com/ocaml/dune/issues/2147) this should \
+         never be the case. Please remove the {build} tag from its filter."
         pkg
 
 let opam_file_path_from_pkg ~repo_dir pkg =