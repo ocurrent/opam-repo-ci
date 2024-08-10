68def2c55af43595b71d230526c8d01fb470d22f
Shon Feder <shon.feder@gmail.com>
2024-08-09 22:34:00 -0400

    Add mli for test.ml

---
+++ lib/test.mli
@@ -0,0 +1,19 @@
+type error = OpamPackage.t * exn
+(** A package and the exception recording its failure to install or pass tests. *)
+
+val error_to_string : error -> string
+
+val test_packages_with_opam : string -> OpamPackage.t list -> error Seq.t
+(** [test_packages_with_opam package revdeps] is the sequence of errors encountered
+    while trying to install and test all the packages in [revdeps].
+
+    Examples:
+
+    {[
+      let no_errors_encountered = assert (test_packages_with_opam cmdliner.1.1.1 revdeps |> Seq.length = 0)
+      let errors_encountered = assert (test_packages_with_opam cmdliner.1.1.1 revdeps |> Seq.length > 0)
+    ]}
+  *)
+
+val test_packages_with_dune :
+  string -> string -> OpamPackage.t list -> (unit, 'a) result
