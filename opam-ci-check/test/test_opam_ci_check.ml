6975fb01fc1aa99a3ce614457642b049540e3a93
Shon Feder <shon.feder@gmail.com>
2024-10-22 13:07:52 -0400

    Add basic test for name collision detection
    At some point soon I think we will want a unit test library, but these
    assertions will do the trick for now.

--- opam-ci-check/test/test_opam_ci_check.ml
+++ opam-ci-check/test/test_opam_ci_check.ml
@@ -1,3 +1,11 @@
 (* SPDX-License-Identifier: Apache-2.0
  * Copyright (c) 2024 Puneeth Chaganti <punchagan@muse-amuse.in>, Shon Feder <shon.feder@gmail.com>, Tarides <contact@tarides.com>
  *)
+
+open Opam_ci_check
+
+let () =
+  (* Detect collision modulo ('-' | '_' | '') *)
+  assert (Lint.Checks.package_name_collision "foo_barbaz" "foo-bar-baz");
+  (* No collision otherwise *)
+  assert (not @@ Lint.Checks.package_name_collision "foo-barbaz" "eoocar_caz")
