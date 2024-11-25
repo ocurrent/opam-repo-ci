7ad88b54f1d9ce010ea1ec1406f9423e9ffc2666
Puneeth Chaganti <punchagan@muse-amuse.in>
2024-11-25 17:48:39 +0530

    Rename repo_dir argument as opam_repo_dir for clarity

--- opam-ci-check/lib/opam_helpers.ml
+++ opam-ci-check/lib/opam_helpers.ml
@@ -4,7 +4,7 @@
 
 let ( // ) = Filename.concat
 
-let path_from_pkg ~repo_dir pkg =
-  repo_dir // "packages"
+let path_from_pkg ~opam_repo_dir pkg =
+  opam_repo_dir // "packages"
   // OpamPackage.Name.to_string (OpamPackage.name pkg)
   // OpamPackage.to_string pkg
