2a3df510466db5e89a1a7b3f370b5a4f8868bb5f
Mark Elvers <mark.elvers@tunbury.org>
2025-07-28 17:21:19 +0000

    Add opam 2.4

--- opam-ci-check/lib/opam_version.ml
+++ opam-ci-check/lib/opam_version.ml
@@ -1,7 +1,8 @@
-type t = [ `Dev | `V2_0 | `V2_1 | `V2_2 | `V2_3] [@@deriving to_yojson]
+type t = [ `Dev | `V2_0 | `V2_1 | `V2_2 | `V2_3 | `V2_4 ] [@@deriving to_yojson]
 
 let to_string = function
   | `Dev -> "dev"
+  | `V2_4 -> "2.4"
   | `V2_3 -> "2.3"
   | `V2_2 -> "2.2"
   | `V2_1 -> "2.1"
